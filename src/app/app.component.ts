58c788ebef64398607a2dc250fef0133cfbd8ca9
Hamid Soleymani <hamid.soleymani@gmx.de>
2021-01-28 01:55:29 +0100

    funktioniert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Inline Template</h1>
+  <app-other></app-other>
+  <app-another>
+    <h1>ich  bin da in another com,</h1>
+  </app-another>
+  `,
+  styles: [`
+  h1{
+    color:red;
+  }
+  `]
 })
 export class AppComponent {
-  title = 'complete-guide-de';
+  title = 'complete-guide-de d333';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
+import { AnotherComponent } from './other/another.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtherComponent,
+    AnotherComponent
   ],
   imports: [
     BrowserModule
