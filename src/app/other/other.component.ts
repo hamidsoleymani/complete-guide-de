a8fcc98a2c68605524c3bd63ab356594cea7de7b
Hamid Soleymani <hamid.soleymani@gmx.de>
2021-01-28 01:54:08 +0100

    funktioniert

---
+++ src/app/other/other.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other',
+  templateUrl: './other.component.html',
+  styles: ['']
+})
+export class OtherComponent {
+
+}
